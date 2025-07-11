4dbbe248de03e2bfeb808ba9e6f7cdd0840a652c
Raza Sayed <raza.sayed@gmail.com>
2025-07-11 21:22:49 +0530

    Created Account model

---
+++ models/account.go
@@ -0,0 +1,50 @@
+package models
+
+import (
+	"bank-api/db"
+	"database/sql"
+	"errors"
+)
+
+var ErrAccountNotFound = errors.New("account not found")
+
+type Account struct {
+	AccountID      int    `json:"account_id"`
+	DocumentNumber string `json:"document_number"`
+}
+
+func GetAccountByID(id int) (*Account, error) {
+	var acc Account
+	err := db.DB.QueryRow(`
+	  SELECT account_id, document_number
+	  FROM accounts
+	  WHERE account_id = $1
+	`, id).Scan(&acc.AccountID, &acc.DocumentNumber)
+
+	switch {
+	case err == sql.ErrNoRows:
+		return nil, ErrAccountNotFound
+	case err != nil:
+		return nil, err
+	default:
+		return &acc, nil
+	}
+}
+
+func CreateAccount(documentNumber string) (*Account, error) {
+	var id int
+	err := db.DB.QueryRow(`
+		INSERT INTO accounts (document_number)
+		VALUES ($1)
+		RETURNING account_id
+	`, documentNumber).Scan(&id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &Account{
+		AccountID:      id,
+		DocumentNumber: documentNumber,
+	}, nil
+}
